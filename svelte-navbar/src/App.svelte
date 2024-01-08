90fa8cb69fa4de13a0c3155b6ab582f540227c65
Louis-X Michaud <louis.x.michaud@gmail.com>
2024-01-08 16:18:28 -0500

    first commit

---
+++ svelte-navbar/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+import Navbar from "./lib/Navbar.svelte";
+import { navbarData } from "./utils/linkUtil";
+</script>
+
+
+<Navbar data={navbarData}/>
+<main>
+<h1>Consulting Ninja</h1>
+</main>
+
+
